3f79e0acc6cf1bc166e23833b7f856796381f356
Stereoblind89 <51133688+Stereoblind89@users.noreply.github.com>
2019-06-08 15:37:09 +0100

    All rules assessed, extra MFAL values added. Original coding string now exporting correctly

--- CodingStringGenerator.py
+++ CodingStringGenerator.py
@@ -4,12 +4,14 @@ import re
 
 def load_option_codes(mfal_loc):
     df = pd.read_excel(mfal_loc, index_col=None, na_values=['NA'], usecols="A,H")
-    df = df[5:]
+    df = df[5:288]
 
     codes = df.iloc[:, 0]
     codes = codes.tolist()
+    codes.extend(['TWB1', 'RK05', 'PRG1', 'LA01', 'HB00', 'GDO1', 'FPF1', 'HODL'])
     active = df.iloc[:, 1]
     active = active.tolist()
+    active.extend(['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'])
 
     dic = {}
 
@@ -35,14 +37,23 @@ def get_original_coding(original_coding_loc):
     df = pd.read_excel(original_coding_loc, sheet_name=1, index_col=None, na_values=['NA'], usecols="D,N,O")
     df = df[10:]
 
-    df = df.values.tolist()
+    string_value = df.iloc[:, 0]
+    string_value = string_value.tolist()
 
-    for i, value in enumerate(df):
-        no_spaces = str(value).replace(" ", "")
-        no_spaces = str(no_spaces).replace("$", "")
-        df[0][i] = no_spaces
+    did_value = df.iloc[:, 1]
+    did_value = did_value.tolist()
 
-    return df
+    segment_value = df.iloc[:, 2]
+    segment_value = segment_value.tolist()
+
+    for i, value in enumerate(string_value):
+        no_spaces = str(value).replace(' ', '')
+        no_spaces = str(no_spaces).replace('$', '')
+        string_value[i] = no_spaces
+
+    return_set = list(zip(string_value, did_value, segment_value))
+
+    return return_set
 
 
 def process_rule(coding_rule):
@@ -68,20 +79,37 @@ def process_rule(coding_rule):
 
 
 def evaluate_option_rules(loc, codes):
-    df = pd.read_excel(loc, sheet_name=0, index_col=None, na_values=['NA'], usecols="I")
+    df = pd.read_excel(loc, sheet_name=0, index_col=None, na_values=['NA'], usecols="B, C, D, E, I, O")
     df = df[10:]
 
-    df = df.values.tolist()
+    start_byte = df.iloc[:, 0]
+    start_byte = start_byte.tolist()
 
-    for i, value in enumerate(df):
-        rule = str(df[i])[1:-1]
+    start_bit = df.iloc[:, 1]
+    start_bit = start_bit.tolist()
+
+    data_value = df.iloc[:, 2]
+    data_value = data_value.tolist()
+
+    data_length = df.iloc[:, 3]
+    data_length = data_length.tolist()
+
+    aston_rule = df.iloc[:, 4]
+    aston_rule = aston_rule.tolist()
+
+    segment = df.iloc[:, 5]
+    segment = segment.tolist()
+
+    rule_result = []
+
+    for i, value in enumerate(aston_rule):
+        rule = str(aston_rule[i])
         rule = ''.join(rule)
+        codes_in_rule, full_rule = process_rule(rule)
 
-        results, full_rule = process_rule(rule)
-
-        for j, op_code in enumerate(results):
-            if not results[0] == '1' and not results[0] == '0':
-                if str(codes.get(op_code)) == 'None':
+        for j, op_code in enumerate(codes_in_rule):
+            if not codes_in_rule[0] == '1' and not codes_in_rule[0] == '0':
+                if op_code not in codes:
                     dictionary_val = str(0)
                 else:
                     dictionary_val = str(codes.get(op_code))
@@ -90,9 +118,13 @@ def evaluate_option_rules(loc, codes):
                 full_rule = str(full_rule).replace("[", "")
                 full_rule = str(full_rule).replace("]", "")
             else:
-                full_rule = str(results[0])
+                full_rule = str(codes_in_rule[0])
 
-        return eval(str(full_rule))
+        rule_result.append(eval(str(full_rule)))
+
+    coding_string_data = list(zip(start_byte, start_bit, data_value, data_length, rule_result, segment))
+
+    return coding_string_data
 
 
 def edit_original_coding(lines, original):
@@ -100,12 +132,13 @@ def edit_original_coding(lines, original):
     final_string = []
     return final_string
 
+
 option_codes_loc = "1PT MFAL AM800.xlsx"
 coding_table_loc = "HVAC222-HVAC222_HVAC_HSW11-00.08.00-C093-19.14.01.xlsx"
 
 option_codes = load_option_codes(option_codes_loc)
 original_coding = get_original_coding(coding_table_loc)
 
-relevant_lines = evaluate_option_rules(coding_table_loc, option_codes)
-
-final_string = edit_original_coding(relevant_lines, original_coding)
+coding_string_data = evaluate_option_rules(coding_table_loc, option_codes)
+print(original_coding[0][0])
+final_string = edit_original_coding(coding_string_data, original_coding)
